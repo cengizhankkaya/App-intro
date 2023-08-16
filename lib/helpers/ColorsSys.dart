f43ce31f697864410f140b388e9e0c75f2f57279
cengizhankkaya <cengizhan.kaya.1453@gmail.com>
2023-08-16 16:08:37 +0300

    App-intro
    Ön Gösterimle  İle Uygulamya Girmek

---
+++ lib/helpers/ColorsSys.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorSys {
+  static Color primary = Color.fromARGB(255, 0, 0, 0);
+  static Color gray = const Color.fromARGB(137, 137, 137, 1);
+  static Color secoundry = const Color.fromARGB(198, 116, 27, 1);
+  static Color secoundryLight = const Color.fromARGB(226, 185, 141, 1);
+}
